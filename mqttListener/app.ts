485ef2402129f8f8761604c428c58c63854e5e1c
Pawel Warmuth <warmuth.pawel@gmail.com>
2021-01-21 11:20:50 +0100

    removed unnecessary looping throug topics, passed array

--- mqttListener/app.ts
+++ mqttListener/app.ts
@@ -1,26 +1,31 @@
 import { config } from 'dotenv'
-import { connect,IPacket, ISubscriptionGrant } from 'mqtt';
+import { connect, IPacket, ISubscriptionGrant, IClientOptions } from 'mqtt';
 import { env } from 'process';
 config();
 
-let topics = env.TOPICS.split(','); 
-let server = env.SERVER;
-let client = connect(`mqtt://${server}`);
 
-client.on('error', (err:IPacket) => {
+class ClientOptions implements IClientOptions {
+    connectTimeout = 2;
+    keepalive = 2;
+    resubscribe= true;
+}
+
+let topics = env.TOPICS.split(',');
+let server = env.SERVER;
+let client = connect(`mqtt://${server}`,new ClientOptions());
+
+client.on('error', (err: IPacket) => {
     console.log(`Error from server ${server} error message: ${err.cmd} `);
 });
-client.on('connect', (message:IPacket) => {
+
+client.on('connect', (message: IPacket) => {
     console.log(`Connected with server ${server} connectonmessage: ${message.cmd} `);
-    for (const key in topics) {
-        if (Object.prototype.hasOwnProperty.call(topics, key)) {
-            const topic = topics[key];
-            client.subscribe(topic,null,(err:Error,grant:ISubscriptionGrant[]) => {
-                console.log(`Subscription request for: ${topic} result: ${grant[0].topic}`)
-            });
-        }
-    }
+    client.subscribe(topics, null, (err: Error, grant: ISubscriptionGrant[]) => {
+        console.log(`Subscribed to ${grant.length} topics`)
+    });
 });
-client.on('message',(topic:string,payload:Buffer,packet:IPacket) => {
-    console.log(`Recieved message in topic: ${ topic } , payload:${payload.toString()} , packetCommand: ${packet.cmd}`)
-})
+
+client.on('message', (topic: string, payload: Buffer, packet: IPacket) => {
+    console.log(`Recieved message in topic: ${topic} , payload:${payload.toString()} , packetCommand: ${packet.cmd}`)
+});
+
