ec8c1cd955c5b5a070605ad83b6113527801e885
Pawel Warmuth <warmuth.pawel@gmail.com>
2021-01-21 16:25:46 +0100

    Added Docker compose file

--- mqttListener/app.ts
+++ mqttListener/app.ts
@@ -7,21 +7,32 @@ config();
 class ClientOptions implements IClientOptions {
     connectTimeout = 2;
     keepalive = 2;
-    resubscribe= true;
+    resubscribe = true;
 }
 
 let topics = env.TOPICS.split(',');
 let server = env.SERVER;
-let client = connect(`mqtt://${server}`,new ClientOptions());
+let client = connect(`mqtt://${server}`, new ClientOptions());
+
+console.log(`Try to listening on ${server} to topics ${topics.join(',')}`)
 
 client.on('error', (err: IPacket) => {
     console.log(`Error from server ${server} error message: ${err.cmd} `);
 });
 
+client.on('offline', () => {
+    throw Error(`There was an error connecting to the Server ${server}`)
+})
+
 client.on('connect', (message: IPacket) => {
     console.log(`Connected with server ${server} connectonmessage: ${message.cmd} `);
     client.subscribe(topics, null, (err: Error, grant: ISubscriptionGrant[]) => {
-        console.log(`Subscribed to ${grant.length} topics`)
+        if (err !== undefined) {
+            console.error(`Error in subscription ${err.message}`)
+        } else {
+            console.log(`Subscribed to ${grant.length} topics`)
+        }
+
     });
 });
 
