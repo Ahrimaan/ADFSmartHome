cf1524faedb0a8292e33c55e375bde89acf64ce3
Pawel <warmuth.pawel@gmail.com>
2021-01-31 18:53:52 +0100

    Bugfix battery draw and load

--- e3dcScheduler/influx.py
+++ e3dcScheduler/influx.py
@@ -21,8 +21,8 @@ if dbExist == False:
 def saveData(item):
     buy = int(item['external']) if int(item['external']) > 0 else 0
     sell = int(item['external']) * -1 if int(item['external']) < 0 else 0
-    batDraw = int(item['battery']) if int(item['battery']) > 0 else 0
-    batLoad = int(item['battery']) * -1 if int(item['battery']) > 0 else 0
+    batDraw = int(item['battery']) * -1 if int(item['battery']) < 0 else 0
+    batLoad = int(item['battery']) if int(item['battery']) > 0 else 0
     data = [{
         "measurement": "PV Anlage",
         "time": item["date"],
@@ -33,7 +33,7 @@ def saveData(item):
             "consumption": int(item['consumption']),
             "buy": buy,
             "sell": sell,
-            "battery": int(item['battery']),
+            "battery": int(item['batteryLoad']),
             "tracker_voltage_1": int(item['tracker_voltage_1']),
             "tracker_voltage_2": int(item['tracker_voltage_2']),
             "tracker_current_1": int(item['tracker_current_1']),
